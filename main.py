3f11e86712a9d82f220818a84329558d965c75a5
Sokol1001 <129304554+Sokol1001@users.noreply.github.com>
2025-05-02 21:18:39 +0300

    Implemented a table of English questions

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ myCursor = db.cursor()
 
 def main():
     print("x = " + vertical_fraction("-b +/- sqrt(b^2 - 4ac)", "2a"))
-    create_databases()
+    create_tables()
 
 #Make fractions look like real math equations    
 def vertical_fraction(numerator, denominator):
@@ -21,11 +21,28 @@ def vertical_fraction(numerator, denominator):
     bottom = str(denominator).center(width)
     return f"{top}\n{line}\n{bottom}"
 
-def create_databases():
+def create_tables():
     #myCursor.execute("CREATE TABLE Test (name varchar(50))")  Create a table
     #myCursor.execute("DROP TABLE Test")  Delete a table
 
-    myCursor.execute("CREATE TABLE English (id int PRIMARY KEY AUTO_INCREMENT, )")
-    db.commit()
+    # myCursor.execute("CREATE TABLE English (" \
+    # " question_id tinyint UNSIGNED," \
+    # " question VARCHAR(500)," \
+    # " option_1 VARCHAR(500)," \
+    # " option_2 VARCHAR(500)," \
+    # " option_3 VARCHAR(500)," \
+    # " option_4 VARCHAR(500)," \
+    # " answer tinyint," \
+    # " id int PRIMARY KEY AUTO_INCREMENT)"
+    # )
+    
+    # --! question_id - 1: Sentence completion 2: Restatements 3: Reading comprehension !--
+
+    #myCursor.execute("INSERT INTO English (question_id, question, option_1, option_2, option_3, option_4, answer) VALUES (%s, %s, %s, %s, %s, %s, %s)", (1, "After being ______ by France for decades, Cambodia became independent in 1953", "bought", "ruled", "translated", "filmed", 2))
+
+    myCursor.execute("SELECT * FROM English")
+
+    for x in myCursor:
+        print(x)
 
 main()
