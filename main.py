b09cc504ba55e8c682d49cef1f7c13d07cf0916e
Sokol1001 <129304554+Sokol1001@users.noreply.github.com>
2025-05-02 19:33:53 +0300

    Day 2

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    passwd = "root",
+    database="testdatabase"
+)
+
+myCursor = db.cursor()
+
 def main():
     print("x = " + vertical_fraction("-b +/- sqrt(b^2 - 4ac)", "2a"))
+    create_databases()
 
 #Make fractions look like real math equations    
 def vertical_fraction(numerator, denominator):
@@ -8,5 +20,12 @@ def vertical_fraction(numerator, denominator):
     line = "-" * width
     bottom = str(denominator).center(width)
     return f"{top}\n{line}\n{bottom}"
+
+def create_databases():
+    #myCursor.execute("CREATE TABLE Test (name varchar(50))")  Create a table
+    #myCursor.execute("DROP TABLE Test")  Delete a table
+
+    myCursor.execute("CREATE TABLE English (id int PRIMARY KEY AUTO_INCREMENT, )")
+    db.commit()
 
 main()
